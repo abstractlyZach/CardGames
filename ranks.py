16ab39f4714004d393a5e0c17e89744929340d8f
Zach Lee <zach.lee@concur.com>
2017-12-13 11:23:28 -0800

    Create get_smaller_ranks()

--- ranks.py
+++ ranks.py
@@ -1,6 +1,12 @@
-RANKS = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+ORDERED_RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
 def get_all_ranks():
-    return RANKS[:]
+    return ORDERED_RANKS[:]
+
+def get_smaller_ranks(rank):
+    '''Gets the ranks that are smaller than the given rank'''
+    rank_index = ORDERED_RANKS.index(rank)
+    return set(ORDERED_RANKS[:rank_index])
+
 
 
--- test_rank.py
+++ test_rank.py
@@ -1,7 +1,7 @@
 
 import ranks
 
-ORDERED_RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+ORDERED_RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
 
 def test_get_all_ranks():
@@ -10,5 +10,20 @@ def test_get_all_ranks():
     for rank in all_ranks:
         assert rank in ORDERED_RANKS
 
-def test_get_smaller_ranks():
-    pass
+def test_get_smaller_ranks_single_rank():
+    rank = 'Ace'
+    smaller_ranks = ranks.get_smaller_ranks(rank)
+    assert len(smaller_ranks) == 12
+
+def test_get_smaller_ranks_lowest_rank_returns_empty_collection():
+    rank = '2'
+    smaller_ranks = ranks.get_smaller_ranks(rank)
+    assert len(smaller_ranks) == 0
+
+def test_get_smaller_ranks_all_ranks():
+    for index, rank in enumerate(ORDERED_RANKS):
+        smaller_ranks = ranks.get_smaller_ranks(rank)
+        for lower_index in range(index):
+            assert ORDERED_RANKS[lower_index] in smaller_ranks
+
+
