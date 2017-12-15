57bdda05f21a49436bf6a991ad26eeedeb0a74fc
Zach Lee <zach.lee@concur.com>
2017-12-14 23:06:34 -0800

    Create script for counting types of hands in poker

---
+++ empirical_hand_probability.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+from deck import StandardPlayingCardDeck
+import poker_hands
+
+poker_hand_counter = defaultdict(int)
+deck = StandardPlayingCardDeck()
+
+hands_to_draw = 5000000
+
+for i in range(hands_to_draw):
+    hand = [deck.draw() for card in range(5)]
+    poker_hand = poker_hands.classify(hand)
+    poker_hand_counter[poker_hand.__class__.__name__] += 1
+    for card in poker_hand:
+        deck.insert_to_bottom(card)
+    deck.shuffle()
+
+
+print('Out of {:,} five-card hands:'.format(hands_to_draw))
+print('{:15} || {:>8} || {:6}'.format('Hand', 'Percent', 'Number of Occurrences'))
+print('=' * 60)
+for poker_hand, occurrences in sorted(poker_hand_counter.items(), key=lambda x:x[1], reverse=True):
+    hand_occurrence_percentage = (occurrences / hands_to_draw) * 100
+    print('{:15} || {:0>7.4f}% || {:8,}'.format(poker_hand, hand_occurrence_percentage, occurrences))
+
