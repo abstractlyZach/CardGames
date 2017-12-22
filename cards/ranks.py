8fc18df4d4be72332ff88c598966ff840eeba166
Zach Lee <zach.lee@concur.com>
2017-12-22 11:21:22 -0800

    Add docstrings for ranks and suits.

--- cards/ranks.py
+++ cards/ranks.py
@@ -17,31 +17,36 @@ ORDERED_RANKS_ACE_HIGH = [TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, 
 ORDERED_RANKS_ACE_LOW = [ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING]
 
 def get_all_ranks():
+    """Return all ranks."""
     return ORDERED_RANKS_ACE_HIGH[:]
 
 def get_smaller_ranks(rank):
-    '''Gets the ranks that are smaller than the given rank'''
+    """Gets the ranks that are smaller than the given rank."""
     rank_index = ORDERED_RANKS_ACE_HIGH.index(rank)
     return ORDERED_RANKS_ACE_HIGH[:rank_index]
 
 def get_larger_ranks(rank):
-    '''Gets the ranks that are larger than the given rank'''
+    """Gets the ranks that are larger than the given rank."""
     rank_index = ORDERED_RANKS_ACE_HIGH.index(rank)
     return ORDERED_RANKS_ACE_HIGH[rank_index + 1:]
 
 def get_royals():
+    """Return the royal ranks."""
     return [TEN, JACK, QUEEN, KING, ACE]
 
 def get_faces():
+    """Return face cards."""
     return [JACK, QUEEN, KING]
 
 def next_larger_rank(rank, ordered_rankings):
+    """Return the next larger rank in the ordering. Raise an exception if it's the highest ranking."""
     rank_index = ordered_rankings.index(rank)
     if rank_index == len(ordered_rankings) - 1:
         raise exceptions.NoHigherRankException
     return ordered_rankings[rank_index + 1]
 
 def get_rank_symbol(rank):
+    """Return the truncated symbol that represents the rank."""
     if rank in [JACK, QUEEN, KING, ACE]:
         return rank[0]
     else:
--- cards/suits.py
+++ cards/suits.py
@@ -11,14 +11,18 @@ SUIT_TO_SYMBOL = {
 }
 
 def get_all_suits():
+    """Return all suits."""
     return SUITS[:]
 
 def get_black_suits():
+    """Return the black suits."""
     return [SPADE, CLOVER]
 
 def get_red_suits():
+    """Return the red suits."""
     return [HEART, DIAMOND]
 
 def get_suit_symbol(suit):
+    """Return the unicode symbol for the given suit."""
     return SUIT_TO_SYMBOL[suit]
 
